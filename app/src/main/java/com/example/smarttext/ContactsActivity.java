b16562eb9fb5abee37bad4ce562127b200a6275e
Jasraj Fulwariya <jasrajfulwariya11223344@gmail.com>
2019-04-07 22:18:48 +0530

    //resolve

--- app/src/main/java/com/example/smarttext/ContactsActivity.java
+++ app/src/main/java/com/example/smarttext/ContactsActivity.java
@@ -3,20 +3,29 @@ import android.content.ContentResolver;
 import android.database.Cursor;
 import android.os.Build;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
 import com.example.smarttext.Adapters.ContactListRecyclerAdapter;
+import com.example.smarttext.utils.Config;
 import com.example.smarttext.utils.ContactData;
 import com.example.smarttext.utils.Permission;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 public class ContactsActivity extends AppCompatActivity {
     private DatabaseReference fireBaseRef;
     private RecyclerView contactRecycler;
+    ArrayList<ContactData> data;
     private ArrayList<ContactData> contactData;
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -29,10 +38,47 @@ public class ContactsActivity extends AppCompatActivity {
             init();
             //TODO: getting Contact Data
             contactData=getContactList();
+            //FilterData
+            final int i=0;
+            data=new ArrayList<>();
+            for(ContactData conData:contactData)
+            {
+                final  ContactData data1=conData;
+                DatabaseReference reference= FirebaseDatabase.getInstance().getReference();
+                reference.child(Config.NODE_ALL_CONTACT).orderByChild(Config.NODE_PHONE_NO)
+                        .equalTo(data1.getPhoneNo()).addListenerForSingleValueEvent(new ValueEventListener() {
+                    int i=0;
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        Log.d("Data", i+" :"+data1.getPhoneNo());
+                        i++;
+                        if(dataSnapshot.exists())
+                        {
+                            //TODO: starting Chat Activity
+                            data.add(data1);
+                            Log.d("Posion Of data", "onDataChange: exists");
+                            String dd=data.get(0).getPhoneNo();
+                            dd="5";
+                        }
+                        else
+                        {
+                            //TODO: Starting Invite Activity
+                            Log.d("Posion Of data ", "onDataChange: not exist");
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            };
             ContactListRecyclerAdapter contactAdapter=new ContactListRecyclerAdapter(this,contactData);
             contactRecycler.setAdapter(contactAdapter);
             contactRecycler.setLayoutManager(new LinearLayoutManager(this));
         }
+
+
     }
 
     private void init() {
