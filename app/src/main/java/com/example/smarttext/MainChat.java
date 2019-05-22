644e56827af394ef44b154302fab644063d51682
Jasraj Fulwariya <jasrajfulwariya11223344@gmail.com>
2019-05-22 12:20:23 +0530

    new 1 22/5/19

--- app/src/main/java/com/example/smarttext/MainChat.java
+++ app/src/main/java/com/example/smarttext/MainChat.java
@@ -1,13 +1,56 @@
 package com.example.smarttext;
 
+import android.content.Intent;
+import android.service.autofill.UserData;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainChat extends AppCompatActivity {
 
+    CircleImageView profile_image;
+    TextView username;
+    FirebaseUser user;
+    DatabaseReference ref;
+    ImageButton messageSend;
+    EditText text_send;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_chat);
+        profile_image=findViewById(R.id.subChatProfilePic);
+        username=findViewById(R.id.subChatMessageSendBox);
+        messageSend=findViewById(R.id.subChatMessageSendBt);
+        username=findViewById(R.id.subChatMessageSendBox);
+        intent=getIntent();
+        String userId=intent.getStringExtra("userid");
+        user= FirebaseAuth.getInstance().getCurrentUser();
+        ref= FirebaseDatabase.getInstance().getReference("AllUser").child(userId);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
